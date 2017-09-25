c8be644dfe0c196714c3570ff4159b21f1cb96ca
DXsmiley <dxsmiley@hotmail.com>
2017-09-25 15:37:50 +1000

    Cleanup the calculator main file

--- mathbot/calculator/__main__.py
+++ mathbot/calculator/__main__.py
@@ -1,15 +1,54 @@
-# Successfull burning attack: f = (x, h) -> if (h - 40, f(x * 2, h + 1) + f(x * 2 + 1, h + 1), 0)
+import argparse
 
 import json
 import asyncio
 import traceback
 import sys
 
-import calculator.interpereter as interpereter
+from calculator.interpereter import Interpereter
 import calculator.parser as parser
 import calculator.bytecode as bytecode
 import calculator.runtime as runtime
 from calculator.runtime import wrap_with_runtime
+
+
+ERROR_TEMPLATE = '''\
+{prev}
+{cur}
+{carat}
+{next}
+'''
+
+
+def main():
+	if len(sys.argv) == 1:
+		interactive_terminal()
+		return
+	# Some options, gotta run file
+	try:
+		args = parse_arguments()
+		filename = proc_filename(args.filename)
+		code = open(filename).read()
+		tokens, ast = parser.parse(code)
+		btc = wrap_with_runtime(bytecode.CodeBuilder(), ast, exportable = True)
+		if args.compile:
+			print(bytecode.stringify(btc))
+			return
+		interpereter = Interpereter(btc, trace = args.trace)
+		result = interpereter.run()
+		print(result)
+	except parser.ParseFailed as e:
+		print(format_parse_error('error', code, e.position))
+
+
+def parse_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filename', help = 'The filename of the program to run')
+	action = parser.add_mutually_exclusive_group()
+	action.add_argument('-t', '--trace', action = 'store_true', help = 'Display details of the program as it is running')
+	action.add_argument('-c', '--compile', action = 'store_true', help = 'Dumps the bytecode of the program rather than running it')
+	return parser.parse_args()
+
 
 def run_with_timeout(future, timeout = None):
 	loop = asyncio.get_event_loop()
@@ -20,13 +59,6 @@ def run_with_timeout(future, timeout = None):
 def print_token_parse_caret(to):
 	print(' '.join(to.tokens))
 	print((sum(map(len, to.tokens[:to.rightmost])) + to.rightmost) * ' ' + '^')
-
-ERROR_TEMPLATE = '''\
-{prev}
-{cur}
-{carat}
-{next}
-'''
 
 
 def format_parse_error(message, string, position):
@@ -50,63 +82,36 @@ def proc_filename(filename):
 	return filename
 
 
-if __name__ == '__main__':
+def interactive_terminal():
 
-	if len(sys.argv) == 1:
+	show_tree = False
+	show_parsepoint = False
+	builder = bytecode.CodeBuilder()
+	runtime = wrap_with_runtime(builder, None)
+	interpereter = Interpereter(runtime, builder = builder)
+	interpereter.run()
 
-		show_tree = False
-		show_parsepoint = False
-		builder = bytecode.CodeBuilder()
-		runtime = wrap_with_runtime(builder, None)
-		interpereter = interpereter.Interpereter(runtime, builder = builder)
-		interpereter.run()
-
-		while True:
-			line = input('> ')
-			if line == '':
-				break
-			elif line == ':tree':
-				show_tree = not show_tree
-			elif line == ':parsepoint':
-				show_parsepoint = not show_parsepoint
-			elif line == ':trace':
-				interpereter.trace = not interpereter.trace
-			else:
-				tokens, ast = parser.parse(line)
-				ast = {'#': 'program', 'items': [ast, {'#': 'end'}]}
-				# print(json.dumps(ast, indent = 4))
-				interpereter.prepare_extra_code({
-					'#': 'program',
-					'items': [ast]
-				})
-				# for index, byte in enumerate(bytes):
-				# 	print('{:3d} - {}'.format(index, byte))
-				print(run_with_timeout(interpereter.run_async(), 5))
-
-	elif len(sys.argv) == 2:
-
-		filename = proc_filename(sys.argv[1])
-		code = open(filename).read()
-		try:
-			tokens, ast = parser.parse(code)
-			btc = wrap_with_runtime(bytecode.CodeBuilder(), ast, exportable = True)
-			# for i, v in enumerate(btc):
-			# 	print('{:3d} {:20}'.format(i, repr(v)))
-			interpereter = interpereter.Interpereter(btc, trace = True)
-			result = interpereter.run()
-			print(result)
-		except parser.ParseFailed as e:
-			print(format_parse_error('error', code, e.position))
-
-	elif len(sys.argv) == 3:
-
-		command = sys.argv[1]
-
-		if command == '-c':
-			filename = proc_filename(sys.argv[2])
-			code = open(filename).read()
-			tokens, ast = parser.parse(code)
-			btc = runtime.wrap_with_runtime(bytecode.CodeBuilder(), ast, exportable = True)
-			print(bytecode.stringify(btc))
+	while True:
+		line = input('> ')
+		if line == '':
+			break
+		elif line == ':tree':
+			show_tree = not show_tree
+		elif line == ':parsepoint':
+			show_parsepoint = not show_parsepoint
+		elif line == ':trace':
+			interpereter.trace = not interpereter.trace
 		else:
-			print('Unknown command: ', command)
+			tokens, ast = parser.parse(line)
+			ast = {'#': 'program', 'items': [ast, {'#': 'end'}]}
+			# print(json.dumps(ast, indent = 4))
+			interpereter.prepare_extra_code({
+				'#': 'program',
+				'items': [ast]
+			})
+			# for index, byte in enumerate(bytes):
+			# 	print('{:3d} - {}'.format(index, byte))
+			print(run_with_timeout(interpereter.run_async(), 5))
+
+
+main()
