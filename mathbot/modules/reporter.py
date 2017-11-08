f9d2dc5b8fb67aea71d7b862d5abc072e28f19cf
DXsmiley <dxsmiley@hotmail.com>
2017-11-08 21:48:31 +1100

    Trying to track down a bug that only happens in production

--- mathbot/modules/reporter.py
+++ mathbot/modules/reporter.py
@@ -18,24 +18,30 @@ class ReporterModule(core.module.Module):
 
 	@core.handles.background_task(requires_ready = True)
 	async def queue_reports(self):
-		while True:
-			if len(QUEUE) > 0:
-				item = QUEUE.pop()
-				await core.keystore.lpush('error-report', item)
-			else:
-				await asyncio.sleep(10)
+		try:
+			while True:
+				if len(QUEUE) > 0:
+					item = QUEUE.pop()
+					await core.keystore.lpush('error-report', item)
+				else:
+					await asyncio.sleep(10)
+		except Exception:
+			print('Exception in queue_reports on shard', self.client.shard_id)
 
 	@core.handles.background_task(requires_ready = True)
 	async def send_reports(self):
-		report_channel = await self.get_report_channel()
-		if report_channel:
-			print('Shard', self.client.shard_id, 'will report errors!')
-			while True:
-				message = await core.keystore.rpop('error-report')
-				if message is not None:
-					await self.client.send_message(report_channel, message)
-				else:
-					await asyncio.sleep(10)
+		try:
+			report_channel = await self.get_report_channel()
+			if report_channel:
+				print('Shard', self.client.shard_id, 'will report errors!')
+				while True:
+					message = await core.keystore.rpop('error-report')
+					if message is not None:
+						await self.client.send_message(report_channel, message)
+					else:
+						await asyncio.sleep(10)
+		except Exception:
+			print('Exception in send_message on shard', self.client.shard_id)
 
 	async def get_report_channel(self):
 		channel_id = core.parameters.get('error-reporting channel')
