7ee491bec78799ee1f312f8940a9af877f817253
DXsmiley <dxsmiley@hotmail.com>
2017-11-12 11:11:49 +1100

    Cleanup parser.py

--- mathbot/calculator/parser.py
+++ mathbot/calculator/parser.py
@@ -1,18 +1,22 @@
 import json
 import re
+import enum
 
 
-BINDING_LEFT = 0
-BINDING_RIGHT = 1
-DROP_DELIMITERS = 2
+class DelimitedBinding(enum.Enum):
+	DONT_PROCESS = 0
+	LEFT_FIRST = 1
+	RIGHT_FIRST = 2
+	DROP_AND_FLATTEN = 3
 
 
 class ParseFailed(Exception):
 
 	def __init__(self, block):
 		r = block.root
-		self.position = min(len(r.original_tokens) - 1, r.rightmost)
-		self.location = r.original_tokens[self.position]['position']
+		self.root = r
+		self.index = min(len(r.original_tokens) - 1, r.rightmost)
+		self.position = r.original_tokens[self.index]['position']
 
 	def __str__(self):
 		return 'Failed to parse token at position {}'.format(self.position)
@@ -43,34 +47,37 @@ class TokenRoot:
 		self.rightmost = max(self.rightmost, position)
 
 	def process_nested_tokens(self, tokens):
+		first = tokens.pop(0)
+		last = tokens.pop()
 		for i in range(len(tokens)):
 			if isinstance(tokens[i], list):
 				tokens[i] = self.process_nested_tokens(tokens[i])
-		return TokenBlock(self, tokens)
+		return TokenBlock(self, tokens, (first, last))
 
 
 class TokenBlock:
 
-	def __init__(self, root, values):
+	def __init__(self, root, values, edge_tokens):
 		self.place = 0
 		self.values = values
 		self.root = root
+		self.edge_tokens = edge_tokens
 
-	def __getitem__(self, index):
-		if self.place + index < len(self.values):
-			token = self.values[self.place + index]
-			if isinstance(token, TokenBlock):
-				return token
-			return token['string']
+	# def __getitem__(self, index):
+	# 	if self.place + index < len(self.values):
+	# 		token = self.values[self.place + index]
+	# 		if isinstance(token, TokenBlock):
+	# 			return token
+	# 		return token['string']
 
-	def eat(self):
-		self.place += 1
-		token = self.values[self.place - 1]
-		if isinstance(token, TokenBlock):
-			return token
-		else:
-			self.root.update_rightmost(token['position'])
-			return token['string']
+	# def eat(self):
+	# 	self.place += 1
+	# 	token = self.values[self.place - 1]
+	# 	if isinstance(token, TokenBlock):
+	# 		return token
+	# 	else:
+	# 		self.root.update_rightmost(token['position'])
+	# 		return token['string']
 
 	def eat_details(self):
 		self.place += 1
@@ -86,8 +93,28 @@ class TokenBlock:
 	def peek(self, index, *valids):
 		if self.place + index < len(self.values):
 			t = self.values[self.place + index]
-			if isinstance(t, dict):
+			if isinstance(t, TokenBlock):
+				return TokenBlock in valids
+			elif isinstance(t, dict):
 				return t['#'] in valids
+
+	def peek_string(self, index, *valids):
+		if self.place + index < len(self.values):
+			t = self.values[self.place + index]
+			if isinstance(t, dict):
+				return t['string'] in valids
+
+	def peek_and_eat(self, index, *valids):
+		assert index == 0
+		if self.peek(index, *valids):
+			return self.eat_details()
+
+
+	def expect(self, index, *valids):
+		r = self.peek(index, *valids)
+		if r is None:
+			raise UnableToFinishParsing(tokens)
+		return r
 
 	def is_complete(self):
 		return self.place >= len(self.values)
@@ -101,6 +128,8 @@ def ensure_completed(function, tokens):
 
 
 def eat_delimited(subrule, delimiters, binding, type, allow_nothing = False, always_package = False):
+	if not isinstance(binding, DelimitedBinding):
+		raise ValueError('{} is not a valid rule for binding'.format(binding))
 	def internal(tokens):
 		listing = []
 		if tokens.is_complete():
@@ -113,7 +142,17 @@ def eat_delimited(subrule, delimiters, binding, type, allow_nothing = False, alw
 			listing.append(subrule(tokens))
 		if len(listing) == 1 and not always_package:
 			return listing[0]
-		if binding == BINDING_LEFT:
+		if binding == DelimitedBinding.DONT_PROCESS:
+			return {
+				'#': type,
+				'items': listing
+			}
+		elif binding == DelimitedBinding.DROP_AND_FLATTEN:
+			return {
+				'#': type,
+				'items': listing[::2]
+			}
+		elif binding == DelimitedBinding.LEFT_FIRST:
 			listing = list(listing[::-1])
 			value = listing.pop()
 			while listing:
@@ -127,7 +166,7 @@ def eat_delimited(subrule, delimiters, binding, type, allow_nothing = False, alw
 					'token': delimiter
 				}
 			return value
-		elif binding == BINDING_RIGHT:
+		elif binding == DelimitedBinding.RIGHT_FIRST:
 			value = listing.pop()
 			while listing:
 				delimiter = listing.pop()
@@ -140,13 +179,6 @@ def eat_delimited(subrule, delimiters, binding, type, allow_nothing = False, alw
 					'token': delimiter
 				}
 			return value
-		elif binding == DROP_DELIMITERS:
-			return {
-				'#': type,
-				'items': listing[::2]
-			}
-		else:
-			raise ValueError
 	internal.__name__ = 'eat_delimited___' + type
 	return internal
 
@@ -162,21 +194,16 @@ def word(tokens):
 
 
 def wrapped_expression(tokens):
-	if isinstance(tokens[0], TokenBlock):
-		return ensure_completed(expression, tokens.eat())
+	if tokens.peek(0, TokenBlock):
+		return ensure_completed(expression, tokens.eat_details())
 	return atom(tokens)
-
-
-def expect(tokens, symbol):
-	assert tokens[0] == symbol
-	return tokens.eat()
 
 
 def function_call(tokens):
 	value = wrapped_expression(tokens)
 	calls = []
-	while isinstance(tokens.details(), TokenBlock):
-		calls.append(ensure_completed(argument_list, tokens.eat()))
+	while tokens.peek(0, TokenBlock):
+		calls.append(ensure_completed(argument_list, tokens.eat_details()))
 	calls = calls[::-1]
 	while calls:
 		value = {
@@ -189,9 +216,10 @@ def function_call(tokens):
 
 def logic_not(tokens):
 	if tokens.peek(0, 'bang'):
-		tokens.eat()
+		token = tokens.eat_details()
 		return {
 			'#': 'not',
+			'token': token,
 			'expression': logic_not(tokens)
 		}
 	return function_call(tokens)
@@ -199,24 +227,30 @@ def logic_not(tokens):
 
 def factorial(tokens):
 	value = logic_not(tokens)
-	while tokens[0] == '!':
-		tokens.eat()
-		value = {'#': 'factorial', 'value': value}
+	while tokens.peek(0, 'bang'):
+		token = tokens.eat_details()
+		value = {
+			'#': 'factorial',
+			'tokens': token,
+			'value': value
+		}
 	return value
 
 
 def dieroll(tokens):
-	if tokens[0] == 'd':
-		tokens.eat()
+	if tokens.peek(0, 'die_op'):
+		token = tokens.eat_details()
 		return {
 			'#': 'die',
+			'token': token,
 			'faces': factorial(tokens)
 		}
 	left = factorial(tokens)
-	if tokens[0] == 'd':
-		tokens.eat()
+	if tokens.peek(0, 'die_op'):
+		token = tokens.eat_details()
 		return {
 			'#': 'die',
+			'token': token,
 			'times': left,
 			'faces': factorial(tokens)
 		}
@@ -244,6 +278,7 @@ def superscript(tokens):
 		result = {
 			'#': 'bin_op',
 			'operator': '^',
+			'token': tok,
 			'left': result,
 			'right': {
 				'#': 'number',
@@ -258,46 +293,51 @@ def expression(tokens):
 	return function_definition(tokens)
 
 
-_parameter_list = eat_delimited(word, ['comma'], DROP_DELIMITERS, 'parameters', allow_nothing = True, always_package = True)
+_parameter_list = eat_delimited(word, ['comma'], DelimitedBinding.DROP_AND_FLATTEN, 'parameters', allow_nothing = True, always_package = True)
+
 
 def parameter_list(tokens):
 	params = _parameter_list(tokens)
 	is_variadic = False
-	if tokens.peek(0, 'period'):
+	if tokens.peek_and_eat(0, 'period'):
 		is_variadic = True
-		tokens.eat()
 	return params, is_variadic
 
 
-argument_list = eat_delimited(expression, ['comma'], DROP_DELIMITERS, 'parameters', allow_nothing = True, always_package = True)
+argument_list = eat_delimited(expression, ['comma'], DelimitedBinding.DROP_AND_FLATTEN, 'parameters', allow_nothing = True, always_package = True)
+
 
 def uminus2(tokens):
-	if tokens[0] == '-':
-		tokens.eat()
+	if tokens.peek_string(0, '-'):
+		token = tokens.eat_details()
 		return {
 			'#': 'uminus',
+			'token': token,
 			'value': uminus2(tokens)
 		}
 	return superscript(tokens)
 
 
-power     = eat_delimited(uminus2,    ['pow_op'],  BINDING_RIGHT, 'bin_op')
+power     = eat_delimited(uminus2,    ['pow_op'],  DelimitedBinding.RIGHT_FIRST, 'bin_op')
+
 
 def uminus(tokens):
-	if tokens[0] == '-':
-		tokens.eat()
+	if tokens.peek_string(0, '-'):
+		t = tokens.eat_details()
 		return {
 			'#': 'uminus',
+			'token': t,
 			'value': uminus(tokens)
 		}
 	return power(tokens)
 
+
 # power     = eat_delimited(uminus,    ['pow_op'],  BINDING_RIGHT, 'bin_op')
-modulo    = eat_delimited(uminus,     ['mod_op'],  BINDING_LEFT,  'bin_op')
-product   = eat_delimited(modulo,    ['mul_op'],  BINDING_LEFT,  'bin_op')
-addition  = eat_delimited(product,   ['add_op'],  BINDING_LEFT,  'bin_op')
-logic_and = eat_delimited(addition,  ['land_op'], BINDING_LEFT,  'bin_op')
-logic_or  = eat_delimited(logic_and, ['lor_op'],  BINDING_LEFT,  'bin_op')
+modulo    = eat_delimited(uminus,    ['mod_op'],  DelimitedBinding.LEFT_FIRST,  'bin_op')
+product   = eat_delimited(modulo,    ['mul_op'],  DelimitedBinding.LEFT_FIRST,  'bin_op')
+addition  = eat_delimited(product,   ['add_op'],  DelimitedBinding.LEFT_FIRST,  'bin_op')
+logic_and = eat_delimited(addition,  ['land_op'], DelimitedBinding.LEFT_FIRST,  'bin_op')
+logic_or  = eat_delimited(logic_and, ['lor_op'],  DelimitedBinding.LEFT_FIRST,  'bin_op')
 
 
 def comparison_list(tokens):
@@ -309,10 +349,11 @@ def comparison_list(tokens):
 			'rest': [],
 		}
 		while tokens.peek(0, 'comp_op'):
-			operator = tokens.eat()
+			token = tokens.eat_details()
 			value = logic_or(tokens)
 			result['rest'].append({
-				'operator': operator,
+				'operator': token['string'],
+				'token': token,
 				'value': value
 			})
 	return result
@@ -320,7 +361,7 @@ def comparison_list(tokens):
 
 def function_definition(tokens):
 	if tokens.peek(1, 'function_definition'):
-		if not isinstance(tokens[0], TokenBlock):
+		if not tokens.peek(0, TokenBlock):
 			raise ParseFailed(tokens)
 		args, is_variadic = ensure_completed(parameter_list, tokens.eat_details())
 		kind = tokens.eat_details()
@@ -339,7 +380,7 @@ def function_definition(tokens):
 def statement(tokens):
 	if tokens.peek(1, 'assignment'):
 		name = word(tokens)
-		tokens.eat()
+		tokens.eat_details()
 		value = expression(tokens)
 		return {
 			'#': 'assignment',
@@ -349,7 +390,7 @@ def statement(tokens):
 	return expression(tokens)
 
 
-program = eat_delimited(statement, ['comma'], DROP_DELIMITERS, 'program')
+program = eat_delimited(statement, ['comma'], DelimitedBinding.DROP_AND_FLATTEN, 'program')
 
 
 def process_tokens(tokens):
@@ -366,18 +407,19 @@ def process_tokens(tokens):
 		raise ImbalancedBraces
 	# Do the thing
 	tokens = tokens[::-1]
-	def recurse():
-		result = []
+	def recurse(first_token):
+		result = [first_token]
 		while tokens:
 			tok = tokens.pop()
 			if tok['string'] == '(':
-				result.append(recurse())
+				result.append(recurse(tok))
 			elif tok['string'] == ')':
+				result.append(tok)
 				break
 			else:
 				result.append(tok)
 		return result
-	return recurse()
+	return recurse(None) + [None]
 
 
 def run(string):
@@ -440,17 +482,6 @@ def tokenizer(original_string, ttypes, source_name = '__unknown__'):
 			location += len(possible[0][1])
 			string = string[len(possible[0][1]):]
 	return result
-
-
-if __name__ == '__main__':
-	# run('1 + 2')
-	# run('( ) -> 4')
-	run_script('c6')
-	run_script('sayaks')
-	# line = input('> ')
-	# while line != '':
-	# 	run(line)
-	# 	line = input('> ')
 
 
 def parse(string, source_name = '__unknown__'):
