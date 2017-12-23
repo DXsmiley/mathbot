b1f964904dba42add2e53137a7d056aecf228f7b
DXsmiley <dxsmiley@hotmail.com>
2017-12-23 12:32:22 +1100

    Improved error handling for the Wolfram|Alpha module

--- mathbot/modules/wolfram.py
+++ mathbot/modules/wolfram.py
@@ -39,13 +39,18 @@ ERROR_MESSAGE_NO_RESULTS = """Wolfram|Alpha didn't send a result back.
 Maybe your query was malformed?
 """
 
+ERROR_MESSAGE_FAILED = '''Failed to make the request.
+Maybe you should try again?
+
+If this error keeps recurring, you should report it to DXsmiley on the \
+official MathBot server: https://discord.gg/JbJbRZS
+'''
+
 ERROR_MESSAGE_TIMEOUT = """Wolfram|Alpha query timed out.
 Maybe you should try again?
 
 If this error keeps recurring, you should report it to DXsmiley on the \
 official MathBot server: https://discord.gg/JbJbRZS
-
-Query: `{}`
 """
 
 ERROR_MESSAGE_ACCOUNT_BLOCKED = """MathBot has exceeded the total number of \
@@ -58,7 +63,8 @@ server: https://discord.gg/JbJbRZS
 FOOTER_MESSAGE = """\
 Query made by {mention}
 Data sourced from Wolfram|Alpha: <http://www.wolframalpha.com/input/?{query}>
-**Do more with Wolfram|Alpha Pro**: <http://www.wolframalpha.com/pro/>
+Do more with Wolfram|Alpha Pro: <http://www.wolframalpha.com/pro/>
+🐺 **Try out the new `=pup` command!** It\'s much more concise.
 """
 
 FOOTER_MESSAGE_SHORT = """\
@@ -317,7 +323,7 @@ class WolframModule(core.module.Module):
 			print('Done?')
 		except asyncio.TimeoutError:
 			print('W|A timeout:', query)
-			await self.send_message(channel, ERROR_MESSAGE_TIMEOUT.format(query), blame = blame)
+			await self.send_message(channel, ERROR_MESSAGE_TIMEOUT, blame = blame)
 		except aiohttp.ClientError as e:
 			print('Wolf: HTTP processing error:', e.message)
 			await self.send_message(channel, 'The server threw an error. Try again in a moment.', blame = blame)
@@ -383,6 +389,8 @@ class WolframModule(core.module.Module):
 			print('Making request')
 			result = await api.request(query, assumptions, debug = debug)
 			print('Done?')
+		except wolfapi.RequestFailed:
+			await self.send_message(channel, ERROR_MESSAGE_FAILED, blame = blame)
 		except asyncio.TimeoutError:
 			print('W|A timeout:', query)
 			await self.send_message(channel, ERROR_MESSAGE_TIMEOUT.format(query), blame = blame)
@@ -442,12 +450,14 @@ class WolframModule(core.module.Module):
 				url = urllib.parse.urlencode({'i': query})
 				# Determine if the footer should be long or short
 				adm = FOOTER_MESSAGE.format(mention = blame.mention, query = url)
-				adm += '🐺 **Try out the new `=pup` command!** It\'s much more concise.'
-				if len(textout_joined) + len(adm) > 1950:
-					adm = FOOTER_MESSAGE_SHORT.format(mention = blame.mention)
+				output = textout_joined + adm
+				too_long = False
+				if len(output) >= 2000:
+					too_long = True
+					output = adm
 				# Send the result
-				posted = await self.send_message(channel, textout_joined + adm, blame = blame)
-				if result.assumptions.count - result.assumptions.count_unknown > 0:
+				posted = await self.send_message(channel, output, blame = blame)
+				if result.assumptions.count - result.assumptions.count_unknown > 0 and not too_long:
 					try:
 						if hidden_assumptions:
 							await self.client.add_reaction(posted, EXPAND_EMOJI)
@@ -478,15 +488,21 @@ class WolframModule(core.module.Module):
 		return '**Assumptions**\n{}\n\n'.format(str(assumptions))
 
 	async def add_reaction_emoji(self, message, assumptions):
-		if assumptions.emoji_count <= MAX_REACTIONS_IN_MESSAGE:
-			for i in range(assumptions.emoji_count):
-				emoji = assumptions.get_emoji(i)
-				if emoji:
-					# Whenever we add a reaction, it automatically exhausts the bucket.
-					# This seems strange, but whatever.
-					await self.client.add_reaction(message, emoji)
-					await asyncio.sleep(0.3)
-		await self.client.add_reaction(message, RERUN_EMOJI)
+		''' Adds assumption emoji to a message. '''
+		try:
+			if assumptions.emoji_count <= MAX_REACTIONS_IN_MESSAGE:
+				for i in range(assumptions.emoji_count):
+					emoji = assumptions.get_emoji(i)
+					if emoji:
+						# Whenever we add a reaction, it automatically exhausts the bucket.
+						# This seems strange, but whatever.
+						await self.client.add_reaction(message, emoji)
+						await asyncio.sleep(0.3)
+			await self.client.add_reaction(message, RERUN_EMOJI)
+		except discord.errors.NotFound:
+			# The message could potentially get deleted part way through
+			# adding emoji. If this happens we should ignore it.
+			print('Message disappeared in the middle of adding reactions.')
 
 
 
