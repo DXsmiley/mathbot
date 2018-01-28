e9685f879d845d01d0a5eea714e5ad0acd454c9d
DXsmiley <dxsmiley@hotmail.com>
2018-01-28 14:17:14 +1100

    Add a =checkallsettings command

--- mathbot/modules/settings.py
+++ mathbot/modules/settings.py
@@ -136,7 +136,7 @@ class SettingsModule(core.module.Module):
 	}.get
 
 	expand_value = {
-		None: '----',
+		None: '--------',
 		1: 'enabled',
 		0: 'disabled',
 		True: 'enabled',
@@ -199,18 +199,26 @@ class SettingsModule(core.module.Module):
 			await self.send_message(message.channel, m, blame = message.author)
 
 
-	# @core.handles.command('checkallsettings'):
-	# async def command_check_all_settings(self, message, arg):
-	# 	class AlternateNone: pass
-	# 	if not message.channel.is_private:
-	# 		lines = []
-	# 		for setting, s_details in core.settings.SETTINGS.items():
-	# 			if 'channel' in s_details['contexts'] and 'server' in s_details['contexts']:
-	# 				lines.append('>>> {} (default: {})'.format(setting, s_details['default']))
-	# 				for channel in message.server.channels:
-	# 					set_to = await core.settings.channel_get_setting(message, setting, 'channel')
-	# 		m = '```\n{}\n```'.format('\n'.join(lines))
-	# 		await self.send_message(message.channel, m, blame = message.author)
+	@core.handles.command('checkallsettings', '')
+	async def command_check_all_settings(self, message):
+		if message.channel.is_private:
+			await self.send_message(message.channel, 'This command cannot be used in private channels.', blame = message.author)
+		else:
+			lines = [' Setting          | Channel  | Server   | Default',
+					 '------------------+----------+----------+----------'
+			]
+			for setting, s_details in core.settings.SETTINGS.items():
+				if 'redirect' not in s_details:
+					value_server  = await core.settings.get_single(setting, message.server)
+					value_channel = await core.settings.get_single(setting, message.channel)
+					lines.append(' {: <16} | {: <8} | {: <8} | {: <8}'.format(
+						setting,
+						SettingsModule.expand_value(value_server),
+						SettingsModule.expand_value(value_channel),
+						SettingsModule.expand_value(s_details['default'])
+					))
+			m = '```\n{}\n```'.format('\n'.join(lines))
+			await self.send_message(message.channel, m, blame = message.author)
 
 
 	@core.handles.command('prefix', '*')
