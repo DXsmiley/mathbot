8b8f849e99721a0178feb976871c8e46848aa65d
DXsmiley <dxsmiley@hotmail.com>
2018-02-15 22:48:45 +1100

    Test discord permission restrictions.

--- mathbot/tests/test_core_manager.py
+++ mathbot/tests/test_core_manager.py
@@ -9,6 +9,16 @@ import discord
 core.keystore.setup_disk(None)
 
 
+class MockManager(core.manager.Manager):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.output_messages = []
+
+	async def send_message(self, destination, content):
+		self.output_messages.append(content)
+
+
 class ExampleModule(core.module.Module):
 
 	@core.handles.command('echo', '*')
@@ -30,23 +40,11 @@ class AnotherModule(core.module.Module):
 		pass # pragma: no cover
 
 
-@pytest.fixture(scope = 'function')
-def manager():
-	return core.manager.Manager('token')
+class SecurityModule(core.module.Module):
 
-
-def test_command_collection(manager):
-	manager.add_modules(ExampleModule(), AnotherModule())
-	manager.setup()
-	assert manager.commands['echo'] == ExampleModule.echo
-	assert manager.commands['hello'] == AnotherModule.hello
-
-
-def test_duplicate_command(manager):
-	manager.add_modules(ExampleModule())
-	manager.add_modules(ConflictingModule())
-	with pytest.raises(core.manager.CommandConflictError):
-		manager.setup()
+	@core.handles.command('test', '', discord_perms='manage_server')
+	async def test(self, message):
+		return 'Permission granted'
 
 
 class MockChannel(discord.Channel):
@@ -63,11 +61,11 @@ class MockServer(discord.Server):
 
 class MockMessage(discord.Message):
 
-	def __init__(self, content):
+	def __init__(self, content, author=None):
 		self.content = content
 		self.channel = MockChannel(False)
 		self.server = MockServer()
-		self.author = MockUser()
+		self.author = author or MockUser()
 
 
 class MockUser: # pylint: disable=too-few-public-methods
@@ -76,9 +74,9 @@ class MockUser: # pylint: disable=too-few-public-methods
 		required to pass the tests.
 	'''
 
-	def __init__(self):
-		self.perms = discord.Permissions.none()
-		self.id = '012345678901234567' # pylint: disable=invalid-name
+	def __init__(self, perms = None):
+		self.perms = perms or discord.Permissions.none()
+		self.id = '1111111111111111' # pylint: disable=invalid-name
 
 	def permissions_in(self, channel):
 		''' Returns the permissions that a user would have if they
@@ -88,8 +86,37 @@ class MockUser: # pylint: disable=too-few-public-methods
 		return self.perms
 
 
+@pytest.fixture(scope = 'function')
+def manager():
+	return MockManager('token')
+
+
 @pytest.mark.asyncio
 async def test_message_handler(manager): # pylint: disable=redefined-outer-name
 	manager.add_modules(ExampleModule())
 	manager.setup()
 	await manager.handle_message(MockMessage('=echo'))
+
+
+def test_command_collection(manager):
+	manager.add_modules(ExampleModule(), AnotherModule())
+	manager.setup()
+	assert manager.commands['echo'] == ExampleModule.echo
+	assert manager.commands['hello'] == AnotherModule.hello
+
+
+def test_duplicate_command(manager):
+	manager.add_modules(ExampleModule())
+	manager.add_modules(ConflictingModule())
+	with pytest.raises(core.manager.CommandConflictError):
+		manager.setup()
+
+
+@pytest.mark.asyncio
+async def test_secure_function(manager):
+	manager.add_modules(SecurityModule())
+	manager.setup()
+	await manager.handle_message(MockMessage('=test', author=MockUser(perms=discord.Permissions.all())))
+	assert manager.output_messages.pop() == 'Permission granted'
+	await manager.handle_message(MockMessage('=test', author=MockUser(perms=discord.Permissions.none())))
+	assert manager.output_messages.pop() != 'Permission granted'
