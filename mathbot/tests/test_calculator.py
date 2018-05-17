0b4b6f3fa62bd5f88b0c23628e1328ac63dbbdb0
DXsmiley <dxsmiley@hotmail.com>
2018-05-17 21:19:41 +1000

    Add tests for some of the calculator standard library

--- mathbot/tests/test_calculator.py
+++ mathbot/tests/test_calculator.py
@@ -4,52 +4,7 @@ import math
 import cmath
 import sympy
 
-TIMEOUT = 10000
-
-class Ignore: pass
-
-def check_string(list, expected):
-	if not isinstance(expected, str):
-		return False
-	assert isinstance(list, calculator.functions.ListBase)
-	assert len(list) == len(expected)
-	for g, c in zip(list, expected):
-		assert g.value == c
-	return True
-
-def doit(equation, expected, use_runtime=False):
-	result = calculator.calculate(equation, tick_limit = TIMEOUT, use_runtime=use_runtime, trace=False)
-	assert expected is Ignore \
-		or isinstance(result, calculator.functions.Glyph) and result.value == expected \
-		or check_string(result, expected) \
-		or result is None and expected is None \
-		or isinstance(result, sympy.boolalg.BooleanAtom) and bool(result) == expected \
-		or sympy.simplify(result - expected) == 0
-
-def dort(equation, expected):
-	doit(equation, expected, use_runtime=True)
-
-def doformatted(equation, expected):
-	result = calculator.calculate(equation, tick_limit = TIMEOUT)
-	formatted = calculator.formatter.format(result)
-	assert formatted == expected
-
-def repeat(equation, start, end):
-	for i in range(20):
-		r = calculator.calculate(equation, tick_limit = TIMEOUT)
-		assert start <= r <= end
-
-def throws(equation):
-	with pytest.raises(calculator.errors.EvaluationError):
-		calculator.calculate(equation, tick_limit = TIMEOUT, use_runtime=True)
-
-def compile_fail(equation):
-	with pytest.raises(calculator.errors.CompilationError):
-		calculator.calculate(equation, tick_limit = TIMEOUT)
-
-def parse_fail(equation):
-	with pytest.raises(calculator.parser.ParseFailed):
-		calculator.calculate(equation, tick_limit = TIMEOUT)
+from test_calc_helpers import *
 
 def test_negation():
 	doit("9", 9)
