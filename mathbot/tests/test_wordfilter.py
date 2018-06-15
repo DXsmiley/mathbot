ffc24594737a12433a097dd3ea8ecd29f1e58b26
DXsmiley <dxsmiley@hotmail.com>
2018-06-15 14:39:10 +1000

    Reword the word filter to handle more edge cases

--- mathbot/tests/test_wordfilter.py
+++ mathbot/tests/test_wordfilter.py
@@ -1,9 +1,14 @@
 import pytest
 from wordfilter import is_bad
 
-def test():
-	assert(not is_bad('Hello, world!'))
-	assert(not is_bad('THESE WORDS ARE FRIENDLY'))
-	assert(is_bad('CRAP'))
-	assert(is_bad('oh fuck this'))
-	assert(is_bad('This is shit.'))
+def test_friendly():
+	assert not is_bad('Hello, world!')
+	assert not is_bad('THESE WORDS ARE FRIENDLY')
+
+def test_malicious():
+	assert is_bad('CRAP')
+	assert is_bad('oh fuck this')
+	assert is_bad('This is shit.')
+
+def test_esoteric():
+	assert is_bad('\u200Bfuck')
--- mathbot/wordfilter/__init__.py
+++ mathbot/wordfilter/__init__.py
@@ -1,22 +1,15 @@
 import string
 import os
 
+
 WORDFILE = os.path.dirname(os.path.abspath(__file__)) + '/bad_words.txt'
+BAD_WORDS = set()
 
-bad_words = set()
-
-# Load bad words from the file
-# Words suffixed with 's' or 'ed' are also considered bad
 with open(WORDFILE) as f:
 	for i in map(str.strip, f):
-		bad_words |= {i, i + 's', i + 'ed'}
+		BAD_WORDS |= {i, i + 's', i + 'ed'}
+
 
 def is_bad(sentence):
-	# Remove all punctuation from the sentence
-	for c in string.punctuation:
-		sentence = sentence.replace(c, ' ')
-	# See if any of the words are bad
-	for word in sentence.lower().split(' '):
-		if word in bad_words:
-			return True
-	return False
+	s = ''.join(map(lambda c: c if c in string.ascii_letters else ' ', sentence))
+	return bool(set(s.lower().split(' ')) & BAD_WORDS)
