fe0374887d7da63f77c1d8d299f65880dfc3d3c2
Ben Mintz <bmintz@protonmail.com>
2018-06-15 00:05:21 -0500

    wordfilter: fix NameError

--- mathbot/wordfilter/__init__.py
+++ mathbot/wordfilter/__init__.py
@@ -1,6 +1,7 @@
 import string
 import os
 
+
 ASCII_LOWERCASE = set(string.ascii_lowercase)  # fast containment check
 WORDFILE = os.path.dirname(os.path.abspath(__file__)) + '/bad_words.txt'
 BAD_WORDS = set()
@@ -9,8 +10,7 @@ with open(WORDFILE) as f:
 	for word in map(str.strip, f):
 		BAD_WORDS |= {word, word + 's', word + 'ed'}
 
-
 def is_bad(sentence):
-	words = s.lower().split()
+	words = sentence.lower().split()
 	words = (''.join(map(lambda c: c if c in ASCII_LOWERCASE else '', word)) for word in words)
 	return bool(set(words) & BAD_WORDS)
