afd9aee641fc272b972b11c8cd58e661e8354957
sayaks <sayaks1@gmail.com>
2018-11-06 15:03:37 +0100

    moved some functions to helpers

--- mathbot/tests/test_calc_helpers.py
+++ mathbot/tests/test_calc_helpers.py
@@ -53,3 +53,33 @@ def compile_fail(equation):
 def parse_fail(equation):
 	with pytest.raises(calculator.parser.ParseFailed):
 		calculator.calculate(equation, tick_limit = TIMEOUT)
+
+def gen_random_deep_list(i=5):
+    l = [0]
+
+    for _ in range(i):
+        l.append(gen_random_deep_list(randint(0,i - 1)))
+    
+    return l
+
+def flatten(x):
+    '''
+    Flattens an irregularly nested list of lists.
+    https://stackoverflow.com/a/2158522
+    '''
+    if isinstance(x, collections.Iterable):
+        return [a for i in x for a in flatten(i)]
+    else:
+        return [x]
+
+
+def joinit(iterable, delimiter):
+    '''
+    Interleave an element into an iterable.
+    https://stackoverflow.com/a/5656097
+    '''
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimiter
+        yield x
