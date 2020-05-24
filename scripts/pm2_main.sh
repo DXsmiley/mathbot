fab34808253168c42e80dabe4b6176329658da0f
DXsmiley <dxsmiley@hotmail.com>
2020-05-24 22:59:41 +1000

    oops

--- scripts/pm2_main.sh
+++ scripts/pm2_main.sh
@@ -5,9 +5,9 @@ set -eux
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-./pull_redis_creds_from_heroku.sh "~/config.json"
-
 cd ~/mathbot
+
+./scripts/pull_redis_creds_from_heroku.sh "~/config.json"
 
 export PIPENV_YES=1
 pipenv install
