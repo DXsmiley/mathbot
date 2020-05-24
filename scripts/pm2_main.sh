980c7591fa7e5ad669518de057bdc239880e4ed0
DXsmiley <dxsmiley@hotmail.com>
2020-05-24 23:06:47 +1000

    Don't require execution perms on that script

--- scripts/pm2_main.sh
+++ scripts/pm2_main.sh
@@ -7,7 +7,7 @@ export LANG=C.UTF-8
 
 cd ~/mathbot
 
-./scripts/pull_redis_creds_from_heroku.sh "../config.json"
+bash ./scripts/pull_redis_creds_from_heroku.sh "../config.json"
 
 export PIPENV_YES=1
 pipenv install
