81c06dd1460d182b01a6e097fcaa8111a29354dd
DXsmiley <dxsmiley@hotmail.com>
2022-08-31 20:30:21 +1000

    Add command to sync global commands

--- mathbot/modules/reboot.py
+++ mathbot/modules/reboot.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import command, Cog
+from discord.ext.commands import command, Cog, Context
 import subprocess
 import modules.reporter
 
@@ -20,6 +20,17 @@ class Reboot(Cog):
 				'-H', 'Accept: application/vnd.heroku+json; version=3',
 				'-H', f'Authorization: Bearer {api_key}'
 			])
+
+	@command()
+	async def sync_commands_global(self, ctx: Context):
+		# TODO: Make this userid set in parameters.json
+		if ctx.author.id == 133804143721578505:
+			await ctx.send('Syncing global commands')
+			async with ctx.typing():
+				print('Syncing global commands...')
+				await ctx.bot.tree.sync()
+				print('Done')
+				await ctx.send('Done')
 
 def setup(bot):
 	return bot.add_cog(Reboot())
